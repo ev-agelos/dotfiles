a63f186b8e8897a161424b939992d10214000b4d
ev-agelos <evagelos.theo@gmail.com>
2017-07-23 16:07:13 +0200

    lock before suspending, improved style

--- i3/shutdown_menu.sh
+++ i3/shutdown_menu.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-res=$(echo "lock|logout|restart|suspend|shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -eh 1 -hide-scrollbar -padding 20 -font "Awesome 10, Hack 15")
+RES=$(echo "lock|logout|restart|suspend|shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -eh 1 -hide-scrollbar -padding 20 -font "Awesome 10, Hack 15")
+LOCK=$HOME/.config/i3/lock.sh
 
-if [ $res = "lock" ]; then
-    $HOME/.config/i3/lock.sh
-fi
-if [ $res = "logout" ]; then
+if [ $RES = "lock" ]; then
+    $LOCK
+elif [ $RES = "logout" ]; then
     i3-msg exit
-fi
-if [ $res = "restart" ]; then
+elif [ $RES = "restart" ]; then
     systemctl reboot
-fi
-if [ $res = "suspend" ]; then
-    systemctl suspend
-fi
-if [ $res = "shutdown" ]; then
+elif [ $RES = "suspend" ]; then
+    $LOCK && systemctl suspend
+elif [ $RES = "shutdown" ]; then
     systemctl poweroff
 fi
 exit 0
