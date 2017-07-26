1a11d7ea0602daa40129ab1302734b6ec3e89173
ev-agelos <evagelos.theo@gmail.com>
2017-07-26 19:53:56 +0200

    add hibernate option in shutdown menu

--- i3/shutdown_menu.sh
+++ i3/shutdown_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RES=$(echo "lock|logout|restart|suspend|shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -eh 1 -hide-scrollbar -padding 20 -font "Awesome 10, Hack 15")
+RES=$(echo "lock|logout|restart|suspend|hibernate|shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -eh 1 -hide-scrollbar -padding 20 -font "Awesome 10, Hack 15")
 LOCK=$HOME/.config/i3/lock.sh
 
 if [ $RES = "lock" ]; then
@@ -11,6 +11,8 @@ elif [ $RES = "restart" ]; then
     systemctl reboot
 elif [ $RES = "suspend" ]; then
     $LOCK && systemctl suspend
+elif [ $RES = "hibernate" ]; then
+    $LOCK && systemctl hibernate
 elif [ $RES = "shutdown" ]; then
     systemctl poweroff
 fi
