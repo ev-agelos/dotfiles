5443abc694112b74dc7954f9126a3622da243ddf
ev-agelos <evagelos.theo@gmail.com>
2017-07-28 21:37:45 +0200

    use i3lock-fancy

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-# If spotify is used uncomment the below line to make it pause when lock..
-# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 
 # suspend notifications
 pkill -u "$USER" -USR1 dunst
@@ -11,8 +6,17 @@ pkill -u "$USER" -USR1 dunst
 amixer set Master mute
 # unmute needs calling i3lock -n which makes hibernation to hang
 
-i3lock -i /tmp/screen.png
-rm /tmp/screen.png
+
+# Old way with pixels
+#scrot /tmp/screen.png
+#convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+#[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+#
+#i3lock -i /tmp/screen.png
+#rm /tmp/screen.png
+
+# Using i3lock-fancy
+$HOME/repos/i3lock-fancy/lock
 
 # resume notifications
 pkill -u "$USER" -USR2 dunst
