be0c8b1a4b9199e65f4ebb50bd55684a9564dc21
ev-agelos <evagelos.theo@gmail.com>
2017-12-10 17:50:54 +0100

    set & use MONITOR before launching polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
+# Launch bar
 polybar example
 
 echo "Bars launched..."
