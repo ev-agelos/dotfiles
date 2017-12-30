4c06b54fc502d6b06a421e2a588cf9fc2294e362
ev-agelos <evagelos.theo@gmail.com>
2017-12-30 10:10:32 +0100

    handle empty/no value/Esc

--- i3/.config/i3/shutdown_menu.sh
+++ i3/.config/i3/shutdown_menu.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-RES=$(echo "lock|logout|suspend|restart|grub reboot:|hibernate|shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -lines 7 -eh 1 -hide-scrollbar -padding 20)
+RES=$(echo "Lock|Suspend|Logout|Restart|Grub index:|Hibernate|Shutdown" | rofi -dmenu -i -sep "|" -p 'Power Menu: ' "" -width 16 -lines 7 -eh 1 -hide-scrollbar -padding 20)
 LOCK=$HOME/.config/i3/lock.sh
 ERROR_MSG="Power menu: invalid input"
 
-if [[ $RES = "lock" ]]; then
+if [[ $RES = "" ]]; then
+    exit 0
+fi
+
+if [[ $RES = "Lock" ]]; then
     $LOCK --nofork
-elif [[ $RES = "logout" ]]; then
+elif [[ $RES = "Logout" ]]; then
     i3-msg exit
-elif [[ $RES = "suspend" ]]; then
+elif [[ $RES = "Suspend" ]]; then
     $LOCK && systemctl suspend
-elif [[ $RES = "restart" ]]; then
+elif [[ $RES = "Restart" ]]; then
     systemctl reboot
-elif [[ $RES = "grub reboot:"* ]]; then
+elif [[ $RES = "Grub index:"* ]]; then
     index=$(echo $RES | cut -d':' -f 2)
     if [[ $index =~ ^[0-9]+$ ]]; then
         gksu grub-reboot $index && systemctl reboot
     else
         notify-send "$ERROR_MSG for grub reboot"
     fi
-elif [[ $RES = "hibernate" ]]; then
+elif [[ $RES = "Hibernate" ]]; then
     $LOCK && systemctl hibernate
-elif [[ $RES = "shutdown" ]]; then
+elif [[ $RES = "Shutdown" ]]; then
     systemctl poweroff
 else
     notify-send "$ERROR_MSG"
 fi
+
 exit 0
