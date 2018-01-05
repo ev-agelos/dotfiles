89718e3618495f714ee12c8fbcdc5c8485343fb6
ev-agelos <evagelos.theo@gmail.com>
2018-01-05 22:44:24 +0100

    use first monitor that polybar finds(to prevent non hdmi as primary)

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,9 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
-
-# Launch bar
-polybar my_bar
+MONITOR=$(polybar -m|head -1|sed -e 's/:.*$//g') polybar my_bar &
 
 echo "Bars launched..."
