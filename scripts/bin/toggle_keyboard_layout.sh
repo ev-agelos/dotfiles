41402d857505926f3bdf265f292838894acea1d2
ev-agelos <evagelos.theo@gmail.com>
2019-01-07 15:09:18 +0100

    scripts: notification and sound when keyboard layout toggles

---
+++ scripts/bin/toggle_keyboard_layout.sh
@@ -0,0 +1,13 @@
+current_layout=$(setxkbmap -query | grep layout | sed 's/layout.* //g')
+
+if [ $current_layout = "us" ]; then
+    new_layout='gr '
+    flag='🇬🇷'
+else
+    new_layout='us ';
+    flag='🇺🇸'
+fi
+
+setxkbmap $new_layout
+notify-send "Keyboard layout:" "$new_layout $flag"
+paplay ~/bin/notification.oga
