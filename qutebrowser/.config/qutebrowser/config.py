96f7bf7f85818ef53695c4a6ab62fd34792fdd38
ev-agelos <evagelos.theo@gmail.com>
2019-11-10 11:22:42 +0100

    qutebrowser: update config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -112,6 +112,38 @@ c.hints.auto_follow = 'always'
 # Type: Int
 c.hints.auto_follow_timeout = 0
 
+# CSS border value for hints.
+# Type: String
+c.hints.border = '1px solid #E3BE23'
+
+# Characters used for hint strings.
+# Type: UniqueCharString
+c.hints.chars = 'asdfghjkl'
+
+# Dictionary file to be used by the word hints.
+# Type: File
+c.hints.dictionary = '/usr/share/dict/words'
+
+# Hide unmatched hints in rapid mode.
+# Type: Bool
+c.hints.hide_unmatched_rapid_hints = True
+
+# Minimum number of characters used for hint strings.
+# Type: Int
+c.hints.min_chars = 1
+
+# Mode to use for hints.
+# Type: String
+# Valid values:
+#   - number: Use numeric hints. (In this mode you can also type letters from the hinted element to filter and reduce the number of elements that are hinted.)
+#   - letter: Use the characters in the `hints.chars` setting.
+#   - word: Use hints words based on the html elements and the extra words.
+c.hints.mode = 'number'
+
+# Leave hint mode when starting a new page load.
+# Type: Bool
+c.hints.leave_on_load = True
+
 # Show a filebrowser in upload/download prompts.
 # Type: Bool
 c.prompt.filebrowser = True
